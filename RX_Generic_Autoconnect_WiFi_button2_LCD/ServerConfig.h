4a0d74480a9c327ac5b0ec9017a7cf5c563a7720
paolometeo <paolometeo@users.noreply.github.com>
2023-01-10 18:40:36 +0100

    Add files via upload

---
+++ RX_Generic_Autoconnect_WiFi_button2_LCD/ServerConfig.h
@@ -0,0 +1,15 @@
+/************************ Adafruit IO Config *******************************/
+//  WiFi network
+// These constants  must be uppercase
+
+//  Connection to the WiFi
+#define WIFI_SSID   ""      
+#define WIFI_PASS   ""
+
+//  Adafruit access
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+/*
+#define IO_USERNAME  "xxxxxx"
+#define IO_KEY       "xxxxxx"
+*/
