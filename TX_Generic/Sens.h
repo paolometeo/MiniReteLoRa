7135b28c9418a18c72a4e5d145ae72c33b4e02d9
paolometeo <paolometeo@users.noreply.github.com>
2023-09-03 16:49:05 +0200

    Delete TX_Generic directory

--- TX_Generic/Sens.h
+++
@@ -1,6 +0,0 @@
-//  Declaration related to the DHT22 sensor
-
-#include "DHT.h"
-#define PINdata 6       // DHT22 data pin
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
- DHT  dht(PINdata,DHTTYPE) ; // Creats a DHT object
--- TX_Generic/SensInit.ino
+++
@@ -1,6 +0,0 @@
-//  put here instructions for initializing sensors 
-// Sensor DHT22
-void SensInit(){
-  dht.begin();
-  altDelay(1000);
-}
